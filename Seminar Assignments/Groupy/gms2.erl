4c5cd2f3d1fc6c338f3abd50cc41a93671848779
Ricard <ricardabril45@gmail.com>
2019-04-30 11:16:16 +0200

    gms2 acabat

--- Seminar Assignments/Groupy/gms2.erl
+++ Seminar Assignments/Groupy/gms2.erl
@@ -1,11 +1,16 @@
 -module(gms2).
 -export([start/1, start/2]).
+-define(timeout,1000).
+
+
 
 start(Name) ->
     Self = self(),
     spawn_link(fun()-> init(Name, Self) end).
 
 init(Name, Master) ->
+    {A1,A2,A3} = now(),
+    rand:seed(A1,A2,A3),
     leader(Name, Master, []).
 
 start(Name, Grp) ->
@@ -13,12 +18,17 @@ start(Name, Grp) ->
     spawn_link(fun()-> init(Name, Grp, Self) end).    
 
 init(Name, Grp, Master) ->
+    {A1,A2,A3} = now(),
+    rand:seed(A1,A2,A3),
     Self = self(), 
     Grp ! {join, Self},
     receive
         {view, Leader, Slaves} ->
+            Ref = erlang:monitor(process,Leader),
             Master ! joined,
-            slave(Name, Master, Leader, Slaves)
+            slave(Name, Master, Leader, Slaves,Ref)
+        after ?timeout -> 
+            Master ! {error,"no reply from leader"}
     end.
 
 leader(Name, Master, Slaves) ->    
@@ -41,21 +51,37 @@ leader(Name, Master, Slaves) ->
 bcast(_, Msg, Nodes) ->
     lists:foreach(fun(Node) -> Node ! Msg end, Nodes).
 
-slave(Name, Master, Leader, Slaves) ->    
+slave(Name, Master, Leader, Slaves, Ref) ->    
     receive
         {mcast, Msg} ->
             Leader ! {mcast, Msg},
-            slave(Name, Master, Leader, Slaves);
+            slave(Name, Master, Leader, Slaves,Ref);
         {join, Peer} ->
             Leader ! {join, Peer},
-            slave(Name, Master, Leader, Slaves);
+            slave(Name, Master, Leader, Slaves,Ref);
         {msg, Msg} ->
             Master ! {deliver,Msg},
-            slave(Name, Master, Leader, Slaves);
+            slave(Name, Master, Leader, Slaves,Ref);
         {view, Leader, NewSlaves} ->
-            slave(Name, Master, Leader, NewSlaves);  %% TODO: COMPLETE
+            slave(Name, Master, Leader, NewSlaves,Ref);  %% TODO: COMPLETE
+        {'DOWN', _Ref, process, Leader, _Reason} ->
+            election(Name, Master, Slaves);
+        {view,NewLeader,NewSlaves} ->
+            erlang:demonitor(Ref,[flush]),
+            NewRef = erlang:monitor(process,NewLeader),
+            slave(Name,Master,NewLeader,NewSlaves,NewRef);
         stop ->
             ok;
         Error ->
             io:format("slave ~s: strange message ~w~n", [Name, Error])
+    end.
+election(Name, Master, Slaves) ->
+    Self = self(),
+    case Slaves of
+        [Self|Rest] ->
+            bcast(Name, {view,Self,Rest},Rest),
+            leader(Name,Master,Rest);
+        [NewLeader|Rest] ->
+            Ref = erlang:monitor(process,NewLeader),
+            slave(Name,Master,NewLeader,Rest,Ref)
     end.
